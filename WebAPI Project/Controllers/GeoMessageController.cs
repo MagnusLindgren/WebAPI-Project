aaca1194c06cf91219a700035bf1b7e5f269c4dc
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-04-29 09:36:56 +0200

    Prepared context and started on a method in controller

--- WebAPI Project/Controllers/GeoMessageController.cs
+++ WebAPI Project/Controllers/GeoMessageController.cs
@@ -3,12 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI_Project.Models;
 
 namespace WebAPI_Project.Controllers
 {
+    [Route("api/geo-comments")]
     [ApiController]
     public class GeoMessageController : ControllerBase
     {
-
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GeoMessage>> GetGeoComment(int id)
+        {
+            var message = await
+            return Ok();
+        }
     }
 }
