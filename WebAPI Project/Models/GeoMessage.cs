beeeca10d86aa61e86767157cca7e6a7b1a1d9e0
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-04-29 09:11:34 +0200

    model GeoMessage

---
+++ WebAPI Project/Models/GeoMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_Project.Models
+{
+    public class GeoMessage
+    {
+     
+            public int id { get; set; }
+            public string message { get; set; }
+            public double longitude { get; set; }
+            public double latitude { get; set; }
+      
+    }
+}
