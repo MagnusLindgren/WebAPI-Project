53b566fce797a0a7efd700b8a7387d8a6f353936
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-04-29 14:45:56 +0200

    started Authentication

---
+++ WebAPI Project/AuthenticationHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace WebAPI_Project
+{
+    public class AuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock,
+            UserManager<IdentityUser> userManager
+            )
+            : base(options, logger, encoder, clock)
+        {
+            _userManager = userManager;
+        }
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            return AuthenticateResult.Fail("Not allowed");
+
+        }
+
+    }
+}
