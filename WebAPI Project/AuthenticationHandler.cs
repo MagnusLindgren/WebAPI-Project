d0be325e44a5f4b6c6f4356639c422a2ad8e7338
Johanna Ljungberg <johanna.ljungberg2@learnet.se>
2021-04-30 14:15:05 +0200

    small adjusment in userManager

--- WebAPI Project/AuthenticationHandler.cs
+++ WebAPI Project/AuthenticationHandler.cs
@@ -10,22 +10,23 @@ using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using WebAPI_Project.Models;
 
 namespace WebAPI_Project
 {
     public class AuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
     {
-        //private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<User> _userManager;
         public AuthenticationHandler(
             IOptionsMonitor<AuthenticationSchemeOptions> options,
             ILoggerFactory logger,
             UrlEncoder encoder,
-            ISystemClock clock
-            // UserManager<IdentityUser> userManager
+            ISystemClock clock,
+             UserManager<User> userManager
             )
             : base(options, logger, encoder, clock)
         {
-            // _userManager = userManager;
+             _userManager = userManager;
         }
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
         {
@@ -35,6 +36,24 @@ namespace WebAPI_Project
                 return AuthenticateResult.Fail("Missing Authorization Header");
 
 
+
+            User user;
+            string password;
+            try
+            {
+                var authenticationHeader = AuthenticationHeaderValue.Parse(Request.Headers["Aruthorication"]);
+                var credentialBytes = Convert.FromBase64String(authenticationHeader.Parameter);
+                var crendentials = Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
+                var username = crendentials[0];
+                password = crendentials[1];
+
+                user = await _userManager.FindByNameAsync(username);
+
+            }
+            catch
+            {
+                return AuthenticateResult.Fail("Invalid Authorization Header");
+            }
 
             var identity = new ClaimsIdentity(Scheme.Name);
             var principal = new ClaimsPrincipal(identity);
@@ -46,31 +65,4 @@ namespace WebAPI_Project
     }
 }
 
-    /*
-     
-            string password;
-            try
-            {
-                var authenticationHeader = AuthenticationHeaderValue.Parse(Request.Headers["Aruthorication"]);
-                var credentialBytes = Convert.FromBase64String(authenticationHeader.Parameter);
-                var crendentials = Encoding.UTF8.GetString(credentialBytes).Split(new[] { ':' }, 2);
-                var username = crendentials[0];
-                password = crendentials[1];
 
-               user = await userManager.FindByNameAsync(username);
-
-            }
-            catch
-            {
-                return AuthenticateResult.Fail("Invalid Authorization Header");
-            }
-        
-            var identity = new ClaimsIdentity(Scheme.Name);
-            var principal = new ClaimsPrincipal(identity);
-            var ticket = new AuthenticationTicket(principal, Scheme.Name);
-            return AuthenticateResult.Success(ticket);
-
-        }
-
-    }
-}*/
